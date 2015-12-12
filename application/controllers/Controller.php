8715893501dd9d95bd84f770bf8dc493cef904e0
bapppa <iftakharul.alam@live.com>
2015-12-13 00:50:24 +0600

    Project Initialized

---
+++ application/controllers/Controller.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\controllers;
+
+use System\core\CI_Controller;
+
+abstract class Controller extends CI_Controller
+{
+
+}
---
+++ application/controllers/Welcome.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\controllers\Controller;
+
+class Welcome extends Controller {
+
+	public function index()
+	{
+		$this->load->view('welcome_message');
+	}
+}
